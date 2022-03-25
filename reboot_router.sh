61d3ac7a7b1ffca82dc95bab032737780e1ab5c9
dreth <github@dac.ac>
2022-03-25 20:33:21 +0100

    add shell script to run the script

---
+++ reboot_router.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# path
+$REBOOT_ROUTER_REPO_LOCATION = "/path/to/reboot_router.py script"
+
+# git pull and run the script
+cd $REBOOT_ROUTER_REPO_LOCATION && git pull && python3 "reboot_router.py" 

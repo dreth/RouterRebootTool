a6588d8aae48a40fcf22887e3c72289e82c734ef
dreth <github@dac.ac>
2022-04-23 11:59:43 +0200

    no git pull before run

--- reboot_router.sh
+++ reboot_router.sh
@@ -4,4 +4,4 @@
 $REBOOT_ROUTER_REPO_LOCATION="/path/to/reboot_router.py script"
 
 # git pull and run the script
-cd $REBOOT_ROUTER_REPO_LOCATION && git pull && python3 "reboot_router.py" 
+cd $REBOOT_ROUTER_REPO_LOCATION && python3 "reboot_router.py" 

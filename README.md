b4e7eb06f51c0f9596f9d2820def5abc66a299f1
dreth <github@dac.ac>
2022-08-09 20:25:27 +0200

    - add more drivers, not just linux - add searching by name - add compatibility for frames - move key params to the top of data.json - add chromedriver - add manual waiting using sleep

--- README.md
+++ README.md
@@ -4,20 +4,33 @@ A simple selenium-based tool to reboot a router by navigating to its configurati
 
 For install instructions (if you need them) go to [Install instructions](#install-instructions), otherwise, you can continue reading.
 
-The only requirements for this to run are at least Python 3.8.8 (I guess it could also work with python 3.8 but I haven't tested) and selenium version `3.141.0`. I've already included a working geckodriver that the script uses.
+The only requirements for this to run are at least Python 3.8.8 (I guess it could also work with python 3.8 but I haven't tested) and selenium version `3.141.0`. I've already included a working geckodriver (and chromedriver) that the script uses. The drivers I include will work on Windows, macOS (arm64) or Linux. 
+
+The driver versions included are:
+- geckodriver 0.31.0 (2022-04-11, b617178ef491)
+- chromedriver 104.0.5112.79
 
 ## How to use
 
 1. Clone this repository.
 
 2. Create a copy of `data.json` and name it `private.json`, this file is deliberately ignored by the `.gitignore` file to avoid being pushed to version control. You should fill up this file as follows:
+   
+    + Under the `driver` key you must choose to either use `geckodriver` or `chromedriver`. E.g. `"driver":"geckodriver"`
+
+    + Under the `address` key, fill up the local address that can access the configuration frontend. This can typically be something like `http://10.0.0.1` or something like this. Don't forget to add `http://`.
+
+    + Under the `headless` key you should type if you want to run it in headless mode (`True`) or not (`False`). If to see the browser interface set this as `False`.
 
     + Under the `credentials` key, fill up the `username` and `password` to log into your router's configuration interface. If there IS a username, then you must write `"True"` under the `exists` key under `username`. If it does NOT have a username, then just write `"False"`
 
     + Under the `path` key you have to fill up several things, to get this information you can use inspect element on your browser after right clicking on the input field for your router username in the login page and then in the html shown by the inspect element menu, right click on the highlighted text and copy what identifier should be used. In general for my purposes, the xpath works well, but you can also use the CSS Selector or ID of the field. After you have the unique identifier then:
   
-      + The `type` field should have the type of identifier you used, either `xpath`, `css_selector`, `class_name` or `id`.
+      + The `type` field should have the type of identifier you used, either `xpath`, `css_selector`, `class_name`, `id` or `name` (`name` is required for frames).
       + The `value` field should have the unique identifier for the field.
+      + If the item is a frame, you must add the key-value `"frame":"True"`
+      + If you want to go back from a frame any number of times to the previous parent element, you must add `"go_to_parent_after":"number_of_times"` where `"number_of_times"` is a number (e.g. "1") if you want to go to the parent *after* interacting with that intermediate element or `"go_to_parent_before":"number_of_times"` in the opposite case, with the same change for `"number_of_times"`.
+      + Optionally, you can add `manually_wait` to an intermediate item so that the script will use `sleep` as opposed to selenium's native `WebDriverWait()`
 
     + Each field key corresponds with the following things:
       + `username` should be the **input field for the router's login username**
@@ -25,10 +38,6 @@ The only requirements for this to run are at least Python 3.8.8 (I guess it coul
       + `login_button` should be the **button to log in**
       + `reboot_submenus` is an array of objects of the same sort as the previous fields. As with most routers, there will be **several intermediate menus until you reach the reboot button** and its confirmation prompt (if present). Here you should add as many `{"type":"", "value":""}` objects as you need for these intermediate menus, in my case I had to click on about 4 links/menus before I reached the reboot button confirmation, so I needed 4 xpaths (because I used xpath) for the values of these 4 intermediate buttons and given that I always used xpath, the `type` key for my 4 intermediate menus was always filled with `xpath`.
       + `reboot_button` is the **final button or link that confirms the reboot**. 
-      
-    + Under the `address` key, fill up the local address that can access the configuration frontend. This can typically be something like `http://10.0.0.1` or something like this. Don't forget to add `http://`.
-
-    + Under the `headless` key you should type if you want to run it in headless mode (`True`) or not (`False`). If to see the browser interface set this as `False`.
 
 3. Run the script in console using `python reboot_router.py` or `python3 reboot_router.py`.
 
--- reboot_router.py
+++ reboot_router.py
@@ -1,9 +1,14 @@
 import json
-from selenium.webdriver import Firefox
-from selenium.webdriver.firefox.options import Options
+from re import M
+from selenium.webdriver import Firefox, Chrome
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from platform import system as current_os_name
+from platform import architecture as current_arch
+from time import sleep
 
 # open json
 with open('private.json', 'r') as f:
@@ -11,13 +16,17 @@ with open('private.json', 'r') as f:
     paths = data['path']
     address = data['address']
     mode = data['headless'].capitalize()
+    driver = data['driver']
+    os_name = current_os_name()
+    arch = current_arch()[0]
 
 # check what to use
 find_by_method = {
     'xpath':'find_element_by_xpath',
     'css_selector':'find_element_by_css_selector',
     'class_name':'find_element_by_class_name',
-    'id':'find_element_by_id'
+    'id':'find_element_by_id',
+    'name':'find_element_by_name'
 }
 
 # check what to wait for
@@ -25,15 +34,21 @@ wait_by = {
     'xpath':'By.XPATH',
     'css_selector':'By.CSS_SELECTOR',
     'class_name':'By.CLASS_NAME',
-    'id':'By.ID'
+    'id':'By.ID',
+    'name':'By.NAME'
 }
 
-# setting options
-opts = Options()
-opts.headless = mode
-
-# defining browser
-browser = Firefox(executable_path="./geckodriver", service_log_path=None, options=opts)
+# defining browser and options
+# FIREFOX
+if driver == 'geckodriver':
+    opts = FirefoxOptions()
+    opts.headless = mode
+    browser = Firefox(executable_path=f"./drivers/{os_name}/{arch}/geckodriver{'.exe' if os_name == 'Windows' else ''}", service_log_path=None, options=opts)
+# CHROME
+elif driver == 'chromedriver':
+    opts = ChromeOptions()
+    opts.headless = mode
+    browser = Chrome(executable_path=f"./drivers/{os_name}/{arch}/chromedriver{'.exe' if os_name == 'Windows' else ''}", service_log_path=None, options=opts)
 
 # wait
 wait = WebDriverWait(driver=browser, timeout=1000)
@@ -42,14 +57,14 @@ wait = WebDriverWait(driver=browser, timeout=1000)
 router_site = browser.get(address)
 
 # wait until login fields show up
-if paths['username']['exists'] == True:
+if paths['username']['exists'].capitalize() == True:
     exec(f"wait.until(EC.visibility_of_element_located(({wait_by[paths['username']['type']]}, paths['username']['value'])))")
 exec(f"wait.until(EC.visibility_of_element_located(({wait_by[paths['password']['type']]}, paths['password']['value'])))")
 exec(f"wait.until(EC.visibility_of_element_located(({wait_by[paths['login_button']['type']]}, paths['login_button']['value'])))")
 print("log in fields are visible.")
 
 # find login fields when they show up and fill them
-if paths['username']['exists'] == True:
+if paths['username']['exists'].capitalize() == True:
     exec(f"browser.{find_by_method[paths['username']['type']]}(paths['username']['value']).click()")
     exec(f"browser.{find_by_method[paths['username']['type']]}(paths['username']['value']).clear()")
     exec(f"browser.{find_by_method[paths['username']['type']]}(paths['username']['value']).send_keys(data['credentials']['username'])")
@@ -64,17 +79,41 @@ print("log in button clicked.")
 for i,current_submenu in enumerate(paths["reboot_submenus"]):
 
     # menu status
-    print(f"intermediate menu {i}")
+    print(f"intermediate menu {i} {', frame' if bool(current_submenu.get('frame')) else ''}")
 
-    # wait until the xpath is visible
-    exec(f"wait.until(EC.visibility_of_element_located(({wait_by[current_submenu['type']]}, current_submenu['value'])))")
+    # BEFORE next step if done with frames in previous section, go to parent 
+    if current_submenu.get("go_to_parent_before"):
+        levels_back = int(current_submenu.get("go_to_parent_before"))
+        print(f'going to parent frame, {levels_back} levels back')
+        for i in range(levels_back):
+            browser.switch_to.default_content()
+
+    # wait until the item is visible
+    # if manually_wait is present, then the timeout is set to the value of that variable and uses sleep instead of selenium's wait
+    if current_submenu.get('manually_wait') and int(current_submenu.get('manually_wait')) > 0:
+        print(f'waiting manually a total of {current_submenu.get("manually_wait")} seconds')
+        sleep(int(current_submenu['manually_wait']))
+    else:
+        exec(f"wait.until(EC.visibility_of_element_located(({wait_by[current_submenu['type']]}, current_submenu['value'])))")
 
     # if the first submenu shows up, the login was successful
     if i == 0:
         print("log in was sucessful.")
 
-    # click the designated xpath
-    exec(f"browser.{find_by_method[current_submenu['type']]}(current_submenu['value']).click()")
+    # if the item is inside a frame, switch to it
+    if bool(current_submenu.get("frame")):
+        print('switching to frame')
+        browser.switch_to.frame(current_submenu['value']) # this seems to only work by name?
+    else:
+        # click the designated item
+        exec(f"browser.{find_by_method[current_submenu['type']]}(current_submenu['value']).click()")
+    
+    # AFTER last click, if done with frames in previous section, go to parent 
+    if current_submenu.get("go_to_parent_after"):
+        levels_back = int(current_submenu.get("go_to_parent_after"))
+        print(f'going to parent frame, {levels_back} levels back')
+        for i in range(levels_back):
+            browser.switch_to.default_content()
 
 # wait until reboot button shows up
 exec(f"wait.until(EC.visibility_of_element_located(({wait_by[paths['reboot_button']['type']]}, paths['reboot_button']['value'])))")
